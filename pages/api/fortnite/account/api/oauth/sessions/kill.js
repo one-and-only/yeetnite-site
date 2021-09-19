70153ffd8f129c52f80b3b6af6aa8d6fab268b1d
Antonios Papadakis <bdpaboi123@gmail.com>
2021-09-19 08:20:56 -0500

    Partial FN API Done

---
+++ pages/api/fortnite/account/api/oauth/sessions/kill.js
@@ -0,0 +1,5 @@
+export default function kill(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'DELETE');
+    res.status(204).send();
+}
