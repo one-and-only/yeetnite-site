8257a90e5dd85573b86a28f5e484ed02be87c47f
Antonios Papadakis <bdpaboi123@gmail.com>
2021-09-24 08:52:54 -0500

    Updates to QueryProfile for Other Profile IDs

--- pages/api/fortnite/fortnite/api/game/v2/profile/queryProfile.js
+++ pages/api/fortnite/fortnite/api/game/v2/profile/queryProfile.js
@@ -2,10 +2,10 @@ export default function queryProfile(req, res) {
     if (req.query.accountId && req.query.profileId && req.query.rvn) {
         const serverTime = new Date().toISOString();
         switch (req.query.profileId) {
+            // TODO make personalization options and 'created' and 'updated' time dynamic (Needs some database work?)
             case 'athena':
                 switch (req.query.rvn) {
                     case '-1':
-                        // TODO make created and updated time dynamic (Needs some database work?)
                         res.json(
                             {
                                 "profileRevision": 1,
@@ -47,6 +47,76 @@ export default function queryProfile(req, res) {
                         });
                         break;
                 }
+                break;
+            case 'common_core':
+                res.json(
+                    {
+                        "profileRevision": 1,
+                        "profileId": "common_core",
+                        "profileChangesBaseRevision": 1,
+                        "profileChanges": [
+                            {
+                                "changeType": "fullProfileUpdate",
+                                "profile": {
+                                    "created": serverTime,
+                                    "updated": serverTime,
+                                    "rvn": 1,
+                                    "wipeNumber": 1,
+                                    "accountId": req.query.accountId,
+                                    "profileId": "common_core",
+                                    "version": "no_version",
+                                    "items": {},
+                                    "stats": {
+                                        "attributes": {
+                                            "banner_color": "defaultcolor1",
+                                            "homebase_name": req.query.accountId,
+                                            "banner_icon": "brseason01"
+                                        }
+                                    },
+                                    "commandRevision": 0
+                                }
+                            }
+                        ],
+                        "profileCommandRevision": 0,
+                        "serverTime": serverTime,
+                        "responseVersion": 1
+                    }
+                );
+                break;
+            case 'common_public':
+                res.json(
+                    {
+                        "profileRevision": 1,
+                        "profileId": "common_public",
+                        "profileChangesBaseRevision": 1,
+                        "profileChanges": [
+                            {
+                                "changeType": "fullProfileUpdate",
+                                "profile": {
+                                    "created": serverTime,
+                                    "updated": serverTime,
+                                    "rvn": 1,
+                                    "wipeNumber": 1,
+                                    "accountId": req.query.accountId,
+                                    "profileId": "common_public",
+                                    "version": "no_version",
+                                    "items": {},
+                                    "stats": {
+                                        "attributes": {
+                                            "banner_color": "defaultcolor1",
+                                            "homebase_name": req.query.accountId,
+                                            "banner_icon": "brseason01"
+                                        }
+                                    },
+                                    "commandRevision": 0
+                                }
+                            }
+                        ],
+                        "profileCommandRevision": 0,
+                        "serverTime": serverTime,
+                        "responseVersion": 1
+                    }
+                );
                 break;
             default:
                 res.status(400).json({
