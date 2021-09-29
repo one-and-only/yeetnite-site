15043494ddb41ec7b9f04fc5803c92087efef8aa
Antonios Papadakis <bdpaboi123@gmail.com>
2021-09-29 09:01:41 -0500

    Fix QueryProfile and Move to JSON

--- pages/api/fortnite/fortnite/api/game/v2/profile/queryProfile.js
+++ pages/api/fortnite/fortnite/api/game/v2/profile/queryProfile.js
@@ -1,122 +1,33 @@
-export default function queryProfile(req, res) {
-    if (req.query.accountId && req.query.profileId && req.query.rvn) {
-        const serverTime = new Date().toISOString();
+import { executeQuery } from '../../../../../../../../includes/db';
+
+export default async function queryProfile(req, res) {
+    if (req.query.accountId && req.query.profileId) {
         switch (req.query.profileId) {
             // TODO make personalization options and 'created' and 'updated' time dynamic (Needs some database work?)
             case 'athena':
-                switch (req.query.rvn) {
-                    case '-1':
-                        res.json(
-                            {
-                                "profileRevision": 1,
-                                "profileId": req.query.profileId,
-                                "profileChangesBaseRevision": 1,
-                                "profileChanges": [
-                                    {
-                                        "changeType": "fullProfileUpdate",
-                                        "profile": {
-                                            "created": serverTime,
-                                            "updated": serverTime,
-                                            "rvn": 1,
-                                            "wipeNumber": 1,
-                                            "accountId": req.query.accountId,
-                                            "profileId": req.query.profileId,
-                                            "version": "no_version",
-                                            "items": {},
-                                            "stats": {
-                                                "attributes": {
-                                                    "banner_color": "defaultcolor1",
-                                                    "homebase_name": req.query.accountId,
-                                                    "banner_icon": "brseason01"
-                                                }
-                                            },
-                                            "commandRevision": 0
-                                        }
-                                    }
-                                ],
-                                "profileCommandRevision": 0,
-                                "serverTime": serverTime,
-                                "responseVersion": 1
-                            }
-                        );
-                        break;
-                    default:
-                        res.status(400).json({
-                            success: false,
-                            reason: `Invalid \`rvn\` of \`${req.query.rvn}\``
-                        });
-                        break;
-                }
+                let athena = require('./profiles/athena.json');
+                const userDataAthena = await executeQuery('SELECT created, lastLogin FROM users WHERE username = ?', [req.query.accountId]);
+                athena.created = userDataAthena[0].created;
+                athena.updated = userDataAthena[0].lastLogin;
+                athena.accountId = req.query.accountId;
+                res.json(athena);
                 break;
             case 'common_core':
-                res.json(
-                    {
-                        "profileRevision": 1,
-                        "profileId": "common_core",
-                        "profileChangesBaseRevision": 1,
-                        "profileChanges": [
-                            {
-                                "changeType": "fullProfileUpdate",
-                                "profile": {
-                                    "created": serverTime,
-                                    "updated": serverTime,
-                                    "rvn": 1,
-                                    "wipeNumber": 1,
-                                    "accountId": req.query.accountId,
-                                    "profileId": "common_core",
-                                    "version": "no_version",
-                                    "items": {},
-                                    "stats": {
-                                        "attributes": {
-                                            "banner_color": "defaultcolor1",
-                                            "homebase_name": req.query.accountId,
-                                            "banner_icon": "brseason01"
-                                        }
-                                    },
-                                    "commandRevision": 0
-                                }
-                            }
-                        ],
-                        "profileCommandRevision": 0,
-                        "serverTime": serverTime,
-                        "responseVersion": 1
-                    }
-                );
+                let common_core = require('./profiles/common_core.json');
+                const userDataCommonCore = await executeQuery('SELECT created, lastLogin FROM users WHERE username = ?', [req.query.accountId]);
+                common_core.created = userDataCommonCore[0].created;
+                common_core.updated = userDataCommonCore[0].lastLogin;
+                common_core.accountId = req.query.accountId;
+                res.json(common_core);
                 break;
             case 'common_public':
-                res.json(
-                    {
-                        "profileRevision": 1,
-                        "profileId": "common_public",
-                        "profileChangesBaseRevision": 1,
-                        "profileChanges": [
-                            {
-                                "changeType": "fullProfileUpdate",
-                                "profile": {
-                                    "created": serverTime,
-                                    "updated": serverTime,
-                                    "rvn": 1,
-                                    "wipeNumber": 1,
-                                    "accountId": req.query.accountId,
-                                    "profileId": "common_public",
-                                    "version": "no_version",
-                                    "items": {},
-                                    "stats": {
-                                        "attributes": {
-                                            "banner_color": "defaultcolor1",
-                                            "homebase_name": req.query.accountId,
-                                            "banner_icon": "brseason01"
-                                        }
-                                    },
-                                    "commandRevision": 0
-                                }
-                            }
-                        ],
-                        "profileCommandRevision": 0,
-                        "serverTime": serverTime,
-                        "responseVersion": 1
-                    }
-                );
+                let common_public = require('./profiles/common_public.json');
+                const userDataCommonPublic = await executeQuery('SELECT created, lastLogin FROM users WHERE username = ?', [req.query.accountId]);
+                common_public.created = userDataCommonPublic[0].created;
+                common_public.updated = userDataCommonPublic[0].lastLogin;
+                common_public.accountId = req.query.accountId;
+                common_public.stats.attributes.homebase_name = req.query.accountId;
+                res.json(common_public);
                 break;
             default:
                 res.status(400).json({
--- pages/api/fortnite/lightswitch/api/service/bulk/status.js
+++ pages/api/fortnite/lightswitch/api/service/bulk/status.js
@@ -1,20 +1,5 @@
+const statusResponse = require('./status.json');
+
 export default function status(req, res) {
-    res.json(
-        [
-            {
-                "serviceInstanceId": "fortnite",
-                "status": "UP",
-                "message": "Fortnite is up",
-                "maintenanceUrl": null,
-                "overrideCatalogIds": [],
-                "allowedActions": [],
-                "banned": false,
-                "launcherInfoDTO": {
-                    "appName": "Fortnite",
-                    "catalogItemId": "fn",
-                    "namespace": "fn"
-                }
-            }
-        ]
-    );
+    res.json(statusResponse);
 }
