b7616257d0528d8c9a8fc67067f587b9c79a2713
Antonios Papadakis <bdpaboi123@gmail.com>
2021-10-24 09:10:34 -0500

    Fix File Name Issue
    Git doesn't track file name updates. I updated next.config.js to match the typo.

--- next.config.js
+++ next.config.js
@@ -125,7 +125,7 @@ module.exports = {
             // ClientQuestLogin
             {
                 source: '/fortnite/api/game/v2/profile/:accountId/client/ClientQuestLogin',
-                destination: '/api/fortnite/fortnite/api/game/v2/profile/clientQuestLogin?accountId=:accountId',
+                destination: '/api/fortnite/fortnite/api/game/v2/profile/ClientQuestLogin?accountId=:accountId',
             },
             // Get or Set ClientSettings.Sav (but saved in the database)
             {
