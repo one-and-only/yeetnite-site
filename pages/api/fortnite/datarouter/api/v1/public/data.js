324c8f05e846cdcf49c66c0ae3783c92d4270efd
Antonios Papadakis <bdpaboi123@gmail.com>
2021-10-24 08:59:48 -0500

    Mimic LawinServer's Requests Better

--- pages/api/fortnite/datarouter/api/v1/public/data.js
+++ pages/api/fortnite/datarouter/api/v1/public/data.js
@@ -1,21 +1,5 @@
-import { cpus } from 'os';
-
 export default function data(req, res) {
     if (req.query.SessionID) {
-        res.json(
-            {
-                "Events": {
-                    "EventName": "Core.LoginFlow",
-                    "DateOffset": "+00:00:32.669", // TODO: I should probably calculate this dynamically?
-                    "GameSessionID": decodeURIComponent(req.query.SessionID),
-                    "Platform": "Windows",
-                    "ProviderType": "Client",
-                    "GameState": "FortGameStateFrontEnd",
-                    "Action": "Back",
-                    // TODO Use the CPU in the request JSON body instead of the server's CPU
-                    "CPU": `${cpus()[0].model} @ ${cpus()[0].speed / 1000}GHz`
-                }
-            }
-        );   
+        res.send();
     }
 }
--- pages/api/fortnite/fortnite/api/cloudstorage/system.js
+++ pages/api/fortnite/fortnite/api/cloudstorage/system.js
@@ -1,5 +1,3 @@
-const systemJson = require('./systems/s6.json');
-
 export default function system(req, res) {
-    res.json(systemJson);
+    res.json([]);
 }
