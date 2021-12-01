e2caadc75b9b1392f719eb625eeda26ffd658762
Antonios Papadakis <43624528+one-and-only@users.noreply.github.com>
2021-12-01 16:42:22 -0600

    Bug Fix
    Fixed a bug where removing a friend didn't resolve the API request. Now all code paths resolve.

--- pages/api/fortnite/account/api/public/friends.js
+++ pages/api/fortnite/account/api/public/friends.js
@@ -7,22 +7,21 @@ export default async function friends(req, res) {
         if (getPendingFriendRequests.length > 0) {
             // since there already is a pending request, it means that we are accepting the request and becoming friends
             await executeQuery("UPDATE friendRequests SET status = 'ACCEPTED' WHERE accountId = ? and ownerAccountId = ?", [req.query.accountId1, req.query.accountId2]);
-            res.status(204).send();
         } else {
             // since there is no pending request, it means that we are sending the request
             const serverTime = new Date().toISOString();
             await executeQuery("INSERT INTO friendRequests (ownerAccountId, accountId, created) VALUES (?, ?, ?)", [req.query.accountId1, req.query.accountId2, serverTime]);
-            res.status(204).send();
         }
+        res.status(204).send();
     } else if (req.method === "DELETE") {
         const pendingFriendRequests = await executeQuery("SELECT * FROM friendRequests WHERE ((ownerAccountId = ? AND accountId = ?) OR (ownerAccountId = ? AND accountId = ?)) AND status = 'PENDING'", [req.query.accountId1, req.query.accountId2, req.query.accountId2, req.query.accountId1]);
         if (pendingFriendRequests.length > 0) {
             // the user is rejecting the request
             await executeQuery("DELETE FROM friendRequests WHERE ((ownerAccountId = ? AND accountId = ?) OR (ownerAccountId = ? AND accountId = ?)) AND status = 'PENDING'", [req.query.accountId1, req.query.accountId2, req.query.accountId2, req.query.accountId1]);
-            res.status(204).send();
         } else {
             // the user is removing a friend
             await executeQuery("DELETE FROM friendRequests WHERE ((ownerAccountId = ? AND accountId = ?) OR (ownerAccountId = ? AND accountId = ?)) AND status = 'ACCEPTED'", [req.query.accountId1, req.query.accountId2, req.query.accountId2, req.query.accountId1]);
         }
+        res.status(204).send();
     }
 }
