7240b9aab7c522635db6e9cd48530915bf5399a3
Antonios Papadakis <bdpaboi123@gmail.com>
2022-06-10 12:45:45 -0500

    Remove Unused Webpack Config Originally used with the `mysql2` library. No longer used now.

--- next.config.js
+++ next.config.js
@@ -239,21 +239,4 @@ module.exports = {
     },
     reactStrictMode: true,
     poweredByHeader: false,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            // don't resolve '...' module on the client
-            // prevents this error on build: Error: Can't resolve '...'
-            config.resolve.fallback = {
-                net: false,
-                tls: false,
-                stream: false,
-                timers: false,
-                crypto: false,
-                zlib: false,
-                cardinal: false,
-            }
-        }
-
-        return config;
-    }
 }
