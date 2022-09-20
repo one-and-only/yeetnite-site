416ba8b7aaf479da1720abfd0007f41aca984eba
Antonios Papadakis <bdpaboi123@gmail.com>
2022-09-20 18:06:40 -0500

    Fix HTTP 500 on Equipping Locker Customization #34

--- pages/api/fortnite/fortnite/api/game/v2/profile/equipBattleRoyaleCustomization.js
+++ pages/api/fortnite/fortnite/api/game/v2/profile/equipBattleRoyaleCustomization.js
@@ -14,7 +14,7 @@ export default async function equipBattleRoyaleCustomization(req, res) {
 
     // "Dance" has an array of values for each of the emote slots
     if (req.body.slotName == "Dance") {
-        profileChanges = JSON.parse((await prisma.$queryRaw`SELECT favorite_dance FROM locker INNER JOIN users ON users.user_id = locker.user_id WHERE users.username = ${req.query.accountId}`).favorite_dance);
+        profileChanges = JSON.parse((await prisma.$queryRaw`SELECT favorite_dance FROM locker INNER JOIN users ON users.user_id = locker.user_id WHERE users.username = ${req.query.accountId}`)[0].favorite_dance);
         profileChanges[req.body.indexWithinSlot] = req.body.itemToSlot;
         profileChanges = JSON.stringify(profileChanges);
     } else {
@@ -22,7 +22,23 @@ export default async function equipBattleRoyaleCustomization(req, res) {
     }
 
     // save the locker changes
-    await prisma.$queryRaw`UPDATE locker SET favorite_${req.body.slotName.toLowerCase()} = ${profileChanges} WHERE user_id IN (SELECT user_id FROM users WHERE username = ${req.query.accountId})`;
+    const user_id = (await prisma.users.findUnique({
+        select: {
+            user_id: true
+        },
+        where: {
+            username: req.query.accountId
+        }
+    })).user_id;
+    let updateParams = {
+        where: {
+            user_id: user_id,
+        },
+        data: {},
+    };
+    updateParams.data[`favorite_${req.body.slotName.toLowerCase()}`] = profileChanges;
+    await prisma.locker.update(updateParams);
+    // await prisma.$queryRaw`UPDATE locker SET favorite_${req.body.slotName.toLowerCase()} = ${profileChanges} WHERE user_id IN (SELECT user_id FROM users WHERE username = ${req.query.accountId})`;
 
     res.json({
         "profileRevision": profileRevision + 1,
@@ -35,7 +51,7 @@ export default async function equipBattleRoyaleCustomization(req, res) {
                 "value": (req.body.slotName == "Dance") ? JSON.parse(profileChanges) : req.body.itemToSlot,
             }
         ],
-        "profileCommandRevision": athena_profile.profileCommandRevision,
+        "profileCommandRevision": require('./profiles/athena.json').profileCommandRevision,
         "serverTime": new Date().toISOString(),
         "responseVersion": 1
     });
